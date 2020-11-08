0cf9a2fa3965c75fc087cfb80540974bb0d7917e
주in <juchan06@naver.com>
2020-11-08 22:00:02 +0900

    Create hangl.py

---
+++ hangl.py
@@ -0,0 +1,8 @@
+import requests
+
+url = 'http://www.medigatenews.com/news/1323371629'
+headers = {'content-type' : 'application/x-www-form-urlencoded; charset=UTF-8', 'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Whale/2.8.108.15 Safari/537.36'}
+data = {'url' : url, 'multiple' : '0'}
+res = requests.post('https://han.gl/shorten', data=data, headers=headers)
+
+print(res.json()['short'])
